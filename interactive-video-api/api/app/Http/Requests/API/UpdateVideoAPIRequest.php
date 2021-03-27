d454d010c3a52f12f024f6578cfc9dac836cb3e5
Thales Farias Martins <thales.martins@grancursosonline.com.br>
2021-03-27 18:01:30 -0300

    feat: Change config for upload

--- interactive-video-api/api/app/Http/Requests/API/UpdateVideoAPIRequest.php
+++ interactive-video-api/api/app/Http/Requests/API/UpdateVideoAPIRequest.php
@@ -9,7 +9,6 @@ class UpdateVideoAPIRequest extends APIRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
      * @return bool
      */
     public function authorize()
@@ -19,12 +18,18 @@ class UpdateVideoAPIRequest extends APIRequest
 
     /**
      * Get the validation rules that apply to the request.
-     *
      * @return array
      */
     public function rules()
     {
         $rules = Video::$rules;
+
+        if ($this->get('_method') == 'PUT') {
+            $rules = [
+                'id_video'   => 'required',
+                'nome_video' => 'required'
+            ];
+        }
 
         return $rules;
     }

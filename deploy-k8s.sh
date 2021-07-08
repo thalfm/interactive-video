5589fa414cd129f086d34b3bac9ba69602740762
Thales Farias Martins <thales.martins@grancursosonline.com.br>
2021-07-08 11:48:00 -0300

    feat: Create manifest for upload socket and catalog

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -1,20 +1,20 @@
 HC='\033[0;32m' # Heading Color
 NC='\033[0m' # No Color
 
-echo -e "${HC}::::::::::::::::::::::::::Pull image interactive-video-api::::::::::::::::::::::::::${NC}"
-docker build /var/www/html/igti/interactive-video-api/ -t ${REPO_API_PHP}
-docker push ${REPO_API_PHP}
-
-echo -e "${HC}::::::::::::::::::::::::::Pull image interactive-video-web::::::::::::::::::::::::::${NC}"
-docker build /var/www/html/igti/interactive-video-web/ -t ${REPO_APP_REACT}
-docker push ${REPO_APP_REACT}
+#echo -e "${HC}::::::::::::::::::::::::::Pull image interactive-video-api::::::::::::::::::::::::::${NC}"
+#docker build /var/www/html/igti/interactive-video-api/ -t ${REPO_API_PHP}
+#docker push ${REPO_API_PHP}
+#
+#echo -e "${HC}::::::::::::::::::::::::::Pull image interactive-video-web::::::::::::::::::::::::::${NC}"
+#docker build /var/www/html/igti/interactive-video-web/ -t ${REPO_APP_REACT}
+#docker push ${REPO_APP_REACT}
 
 echo -e "${HC}::::::::::::::::::::::::::Create environment Data Base Postgres::::::::::::::::::::::::::${NC}"
 kubectl delete -f k8s/pgsql/deploy.yml
 kubectl delete -f k8s/pgsql/svc.yml
 
-kubectl delete secret pgsql-pass
-kubectl create secret generic pgsql-pass --from-literal=password=${DB_PASSWORD}
+#kubectl delete secret pgsql-pass
+#kubectl create secret generic pgsql-pass --from-literal=password=${DB_PASSWORD}
 
 kubectl create -f k8s/pgsql/pvc.yml
 kubectl create -f k8s/pgsql/deploy.yml
